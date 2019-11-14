bb832865bad75fc0f3ef1c0dc5f9f48d517f1dad
EulerSolutionsMatt <matt@eulersolutions.co.za>
2019-11-14 17:06:04 +0200

    add Place pin on current location on map

--- RaceMe/RaceMe/Views/RoutesLogPage.xaml.cs
+++ RaceMe/RaceMe/Views/RoutesLogPage.xaml.cs
@@ -24,40 +24,48 @@ namespace RaceMe.Views
          * Gets the users last known location &reverse geocode it into GPS coords
          */
         async void Reverse_Geocode_Current_Location(object sender, System.EventArgs e)
-        {  
+        {
             try
             {
 
                 // get current user location
                 var current_location = await Geolocation.GetLastKnownLocationAsync();
 
-                // position map over users current location
-                MyMap.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(current_location.Latitude, current_location.Longitude),
-                                             Distance.FromMiles(1)));
-
-
                 // reverse geocode current_location 
                 var locations = await Geocoding.GetPlacemarksAsync(current_location.Latitude, current_location.Longitude);
-                
+
                 // get the first location
                 var first_location = locations?.FirstOrDefault();
 
-                if (first_location != null)
-                {
-                    var geocodeAddress =
-                        $"AdminArea:       {first_location.AdminArea}\n" +
-                        $"CountryCode:     {first_location.CountryCode}\n" +
-                        $"CountryName:     {first_location.CountryName}\n" +
-                        $"FeatureName:     {first_location.FeatureName}\n" +
-                        $"Locality:        {first_location.Locality}\n" +
-                        $"PostalCode:      {first_location.PostalCode}\n" +
-                        $"SubAdminArea:    {first_location.SubAdminArea}\n" +
-                        $"SubLocality:     {first_location.SubLocality}\n" +
-                        $"SubThoroughfare: {first_location.SubThoroughfare}\n" +
-                        $"Thoroughfare:    {first_location.Thoroughfare}\n";
+                var geocodeAddress =
+                    $"AdminArea:       {first_location.AdminArea}\n" +
+                    $"CountryCode:     {first_location.CountryCode}\n" +
+                    $"CountryName:     {first_location.CountryName}\n" +
+                    $"FeatureName:     {first_location.FeatureName}\n" +
+                    $"Locality:        {first_location.Locality}\n" +
+                    $"PostalCode:      {first_location.PostalCode}\n" +
+                    $"SubAdminArea:    {first_location.SubAdminArea}\n" +
+                    $"SubLocality:     {first_location.SubLocality}\n" +
+                    $"SubThoroughfare: {first_location.SubThoroughfare}\n" +
+                    $"Thoroughfare:    {first_location.Thoroughfare}\n";
 
-                    await DisplayAlert("Alert", geocodeAddress, "OK");
-                }
+                await DisplayAlert("Alert", geocodeAddress, "OK");
+
+
+                // add a pin on the map at the users current location
+                Pin pin = new Pin
+                {
+                    Type = PinType.Place,
+                    Position = new Position(current_location.Latitude, current_location.Longitude),
+                    Label = "Your Location",
+                    Address = geocodeAddress
+                };
+
+                MyMap.Pins.Add(pin);
+
+                // position map over users current location
+                MyMap.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(current_location.Latitude, current_location.Longitude),
+                                             Distance.FromMiles(1)));
 
             }
             catch (FeatureNotSupportedException fnsEx)
@@ -70,7 +78,7 @@ namespace RaceMe.Views
             }
 
 
-           
+
 
         }
 
